1dba94202fcca361a527cdbc7bc695761bb10cd8
lukasobg <lukas.olenborg@aalto.fi>
2023-09-25 14:09:10 +0300

    fixed rho

--- state_eqs.m
+++ state_eqs.m
@@ -15,15 +15,16 @@ function dy = state_eqs(y, CL, alpha, CD0, K, S, m, g)
           (L - m*g*cos(gamma)) / (v*m);];
 end
 
-function L = total_lift(CL, alpha, v)
-    L = CL*alpha*S*q(v);
+function L = total_lift(CL, alpha, rho, v)
+    q = air_density(rho,v);
+    L = CL*alpha*S*q;
 end
 
-function D = total_drag(CD0, K, CL, alpha, S, v)
-    q = air_density(v);
+function D = total_drag(CD0, K, CL, alpha, S, rho, v)
+    q = air_density(rho,v);
     D = (CD0 + K*(CL*alpha)^2)*S*q;
 end
 
-function q = air_density(v)
+function q = air_density(rho,v)
     q = 0.5*rho*v^2;
 end
